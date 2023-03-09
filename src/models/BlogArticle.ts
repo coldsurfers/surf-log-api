33a521cc2d9f1a9d3c069b71028f3ab4b22fafbd
yungblud <killi8n@gmail.com>
2023-03-09 11:44:41 +0900

    feat: :zap: added BlogArticle functions

--- src/models/BlogArticle.ts
+++ src/models/BlogArticle.ts
@@ -1,3 +1,4 @@
+import { prisma } from '../database/instance'
 import BlogArticleCategory from './BlogArticleCategory'
 
 export default class BlogArticle {
@@ -19,4 +20,58 @@ export default class BlogArticle {
   >
 
   public createdAt!: Date
+
+  public static async findByExcerpt(excerpt: string) {
+    try {
+      return await prisma.blogArticle.findUnique({
+        where: {
+          excerpt,
+        },
+      })
+    } catch (e) {
+      console.error(e)
+      return null
+    }
+  }
+
+  public static async list({
+    page,
+    category,
+    tag,
+    count = 15,
+  }: {
+    page: number
+    category?: string
+    tag?: string
+    count?: number
+  }) {
+    try {
+      return await prisma.blogArticle.findMany({
+        where: {
+          blogArticleCategory: category
+            ? {
+                name: category,
+              }
+            : undefined,
+          blogArticleTags: tag
+            ? {
+                some: {
+                  blogArticleTag: {
+                    name: tag,
+                  },
+                },
+              }
+            : undefined,
+        },
+        orderBy: {
+          createdAt: 'desc',
+        },
+        take: count,
+        skip: (page - 1) * count,
+      })
+    } catch (e) {
+      console.error(e)
+      return null
+    }
+  }
 }
