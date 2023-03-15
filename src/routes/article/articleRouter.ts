bd279d6168856e604af5a8e3d93b3129edbbe186
yungblud <killi8n@gmail.com>
2023-03-15 20:18:42 +0900

    feat: :zap: allowed ip

--- src/routes/article/articleRouter.ts
+++ src/routes/article/articleRouter.ts
@@ -6,11 +6,21 @@ import {
   saveBlogArticleHandler,
 } from './articleHandler'
 
+const { ALLOWED_ADMIN_IP } = process.env
+
 const articleRouter: FastifyPluginCallback = async (fastify, opts, done) => {
   fastify.get('/:excerpt', blogArticleByExcerptHandler)
   fastify.delete('/:excerpt', deleteArticleByExcerptHandler)
   fastify.get('/list', blogArticleListRouteHandler)
-  fastify.post('/save', saveBlogArticleHandler)
+  fastify.post('/save', {
+    onRequest: async (req, rep, next) => {
+      if (req.ip !== ALLOWED_ADMIN_IP) {
+        return rep.status(404).send()
+      }
+      return next()
+    },
+    handler: saveBlogArticleHandler,
+  })
   done()
 }
 
--- src/routes/file/fileRouter.ts
+++ src/routes/file/fileRouter.ts
@@ -1,8 +1,18 @@
 import { FastifyPluginCallback } from 'fastify'
 import { fileUploadHandler } from './fileHandler'
 
+const { ALLOWED_ADMIN_IP } = process.env
+
 const fileRouter: FastifyPluginCallback = async (fastify, opts, done) => {
-  fastify.get('/:directory', fileUploadHandler)
+  fastify.get('/:directory', {
+    onRequest: async (req, rep, next) => {
+      if (req.ip !== ALLOWED_ADMIN_IP) {
+        return rep.status(404).send()
+      }
+      return next()
+    },
+    handler: fileUploadHandler,
+  })
   done()
 }
 
